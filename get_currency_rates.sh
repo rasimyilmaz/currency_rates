30047619b6e837676d107a2c647a47ae34dacdb1
Rasim Yılmaz <iletisim@rasimyilmaz.com>
2023-06-05 20:06:40 +0300

    Update get_currency_rates.sh

--- get_currency_rates.sh
+++ get_currency_rates.sh
@@ -1,2 +1,2 @@
 #http://www.compciv.org/recipes/cli/downloading-with-curl/
-curl https://www.tcmb.gov.tr/kurlar/today.xml --output /var/www/html/currency_rates/today.xml --slient
+curl https://www.tcmb.gov.tr/kurlar/today.xml --output /var/www/html/currency_rates/today.xml -s
