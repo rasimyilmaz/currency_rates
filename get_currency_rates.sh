47ff80f0a26c07aec33ac7860f2d2c3e851158e9
Rasim Yılmaz <iletisim@rasimyilmaz.com>
2023-06-13 09:30:28 +0300

    Update get_currency_rates.sh

--- get_currency_rates.sh
+++ get_currency_rates.sh
@@ -1,2 +1,4 @@
 #http://www.compciv.org/recipes/cli/downloading-with-curl/
+#https://unix.stackexchange.com/questions/463807/how-to-clear-chromium-browser-cache
+rm -rf ~/.cache/chromium
 curl https://www.tcmb.gov.tr/kurlar/today.xml --output /var/www/html/currency_rates/today.xml -s
